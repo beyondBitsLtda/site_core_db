2057ab8307ec8817eab47de7662d93e01ad719eb
beyondBitsLtda <beyondbitsltda@gmail.com>
2025-12-03 20:39:45 -0300

    Subindo ajustes

--- js/config.js
+++ js/config.js
@@ -2,11 +2,11 @@
 // Os valores são reconstruídos apenas em tempo de execução.
 const contactConfig = {
   emails: {
-    primary: 'YnJheWFuLnJvZHJpZ3Vlc0Bjb3JlZGIuY29tLmJy', // base64 de brayan.rodrigues@coredb.com.br
-    cc: 'aGVucmlxdWUuYXJhdWpvQGNvcmVkYi5jb20uYnI=', // base64 de henrique.araujo@coredb.com.br
+    primary: 'YnJheWFuLnJvZHJpZ3Vlc0Bjb3JlZGIuY29tLmJy',
+    cc: 'aGVucmlxdWUuYXJhdWpvQGNvcmVkYi5jb20uYnI=',
   },
   whatsapp: {
-    phone: 'NTUzMTk5MDgyMjczNA==', // base64 de 5531990822734
+    phone: 'NTUzODk5ODYyMjQyOA==',
   },
 };
 
